29d182f3afc59ac4cf429366c2992936ab98006f
Bill Konchellah <williamoriginal2019@gmail.com>
2021-12-01 18:20:50 +0300

    "Completed part 3"

--- api/app.js
+++ api/app.js
@@ -1,9 +1,12 @@
 express = require('express');
+router = require('./src/router')
 path = require('path');
 
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+app.use('/',router);
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 app.use('/*',(req, res) => {
     res.sendFile(pathToIndex);
 });
---
+++ api/src/router.js
@@ -0,0 +1,29 @@
+const { Router } = require('express');
+const storage = require('multer').diskStorage({destination: 'api/uploads/',filename:filename});
+
+const router = Router();
+
+function filename(request,file,callback){
+    callback(null,file.originalname);
+}
+
+function fileFilter(request,file,callback){
+    if(file.mimetype != 'image/png'){
+        request.fileValidationError = 'Wrong file  type';
+        callback(null,false, new Error('Wrong file type'));
+    }else{
+        callback(null,true);
+    }
+}
+
+const upload = multer({storage:storage,fileFilter:fileFilter});
+
+router.post('/upload',upload.single('photo'),(request,response)=>{
+    if(request.fileValidationError){
+        response.status(400).json({error:request.fileValidationError});
+    }else {
+        response.status(201).json({sucess:true});
+    }
+});
+
+module.exports = router;
