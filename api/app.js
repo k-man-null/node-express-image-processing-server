62464ef5eb29485a083fd6ada941c9543fef93b3
Bill Konchellah <williamoriginal2019@gmail.com>
2021-12-01 17:07:31 +0300

    "Image upload page"

---
+++ api/app.js
@@ -0,0 +1,11 @@
+express = require('express');
+path = require('path');
+
+const app = express();
+
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+app.use('/*',(req, res) => {
+    res.sendFile(pathToIndex);
+});
+
+module.exports = app;
